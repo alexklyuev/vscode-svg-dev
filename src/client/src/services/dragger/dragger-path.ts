a5ade31eb8906f1269c8e65bdaea7bbf1440a8a2
Алексей Клюев <kontrail@MacBook-Pro-Aleksej.local>
2019-06-17 23:52:52 +0300

    Dragger fixes

--- src/client/src/services/dragger/dragger-path.ts
+++ src/client/src/services/dragger/dragger-path.ts
@@ -1,9 +1,14 @@
 import { Dragger } from "./dragger.interface";
+import { Zoom } from "../zoom/zoom";
 
 
 
 export class DraggerPath implements Dragger {
-    
+
+    constructor(
+        private zoom: Zoom,
+    ) {}
+
     onMousedown() {}
 
     onMousemove() {}
