daf907d0f08434e0fc0e38506676e39594dda77e
Алексей Клюев <kontrail@MacBook-Pro-Aleksej.local>
2019-06-30 22:08:18 +0300

    fix ellipse points

--- src/client/src/figures/ellipse.figure.ts
+++ src/client/src/figures/ellipse.figure.ts
@@ -7,6 +7,8 @@ import { CancelListener } from "../listeners/cancel.listener";
 import { UserEventManager } from "../services/user-event/user-event-manager";
 import { ArtboardMove } from "../services/artboard/artboard-move";
 import { Guides } from "../services/guides/guides";
+import { PointConcerns } from "./models/point-concerns.model";
+import { Coorinator } from "../services/coordinator/coordinator";
 
 export class EllipseFigure implements Figure<SVGEllipseElement> {
 
@@ -22,6 +24,7 @@ export class EllipseFigure implements Figure<SVGEllipseElement> {
         public readonly cancelListener: CancelListener,
         public readonly userEventMan: UserEventManager,
         public readonly guides: Guides,
+        private coords: Coorinator,
     ) {
     }
 
@@ -31,44 +34,47 @@ export class EllipseFigure implements Figure<SVGEllipseElement> {
 
     @setState
     create(_elementName: string, _attributes: {[K: string]: string}): void {
-        let points = Array<[number, number]>();
+        let points = Array<PointConcerns>();
         this.artboard.box.classList.add('interactive-points');
         let pseudoElement: SVGEllipseElement | null = null;
         this.userEventMan.mode = 'interactive';
         const pointsListener = (event: MouseEvent) => {
             let { clientX, clientY, shiftKey } = event;
             const { scrollLeft, scrollTop } = document.scrollingElement!;
-            // const { top: moveTop, left: moveLeft } = this.artboardMove;
-            if (points.length === 1 && shiftKey) {
-                let [ [prevX, prevY] ] = points;
-                prevX -= scrollLeft;
-                prevY -= scrollTop;
-                const absDeltaX = Math.abs(clientX - prevX);
-                const absDeltaY = Math.abs(clientY - prevY);
-                if (absDeltaX < absDeltaY) {
-                    clientX = points[0][0] - scrollLeft;
-                } else {
-                    clientY = points[0][1] - scrollTop;
-                }
-            }
-            points.push([
-                clientX + scrollLeft - this.artboardMove.left,
-                clientY + scrollTop - this.artboardMove.top,
-            ]);
+            points.push({
+                client: [clientX, clientY],
+                scroll: [scrollLeft, scrollTop],
+                margin: [this.artboardMove.left, this.artboardMove.top],
+                board: [this.artboard.width, this.artboard.height],
+                zoom: this.zoom.value,
+            });
             if (points.length === 1) {
-                pseudoElement = this.createEditingSelection(points);
+                pseudoElement = this.createEditingSelection(points[0]);
             }
             if (points.length >= 2) {
                 cancel();
-                const [ [x1, y1], [x2, y2] ] = points;
-                const ellipse = document.createElementNS('http://www.w3.org/2000/svg', this.name);
-                ellipse.setAttribute('cx', `${ (x2 - x1) / 2 + x1 }`);
-                ellipse.setAttribute('cy', `${ (y2 - y1) / 2 + y1 }`);
-                ellipse.setAttribute('rx', `${ (x2 - x1) / 2 }`);
-                ellipse.setAttribute('ry', `${ (y2 - y1) / 2 }`);
-                ellipse.setAttribute('stroke', '#ffffff');
-                ellipse.setAttribute('fill', '#ccc');
-                this.artboard.svg.appendChild(ellipse);
+                const [ [x1, y1], [x2, y2] ] = points.map(({ client, scroll, margin, board, zoom }) => {
+                    return this.coords.formula2d(client, scroll, margin, board, zoom, true);
+                });
+                const element = document.createElementNS('http://www.w3.org/2000/svg', this.name);
+                const cx = Math.abs(x2 - x1) / 2 + (x2 > x1 ? x1 : x2);
+                const cy = Math.abs(y2 - y1) / 2 + (y2 > y1 ? y1 : y2);
+                let rx = Math.abs(x2 - x1) / 2;
+                let ry = Math.abs(y2 - y1) / 2;
+                if (shiftKey) {
+                    if (rx > ry) {
+                        rx = ry;
+                    } else {
+                        ry = rx;
+                    }
+                }
+                element.setAttribute('cx', `${ cx }`);
+                element.setAttribute('cy', `${ cy }`);
+                element.setAttribute('rx', `${ rx }`);
+                element.setAttribute('ry', `${ ry }`);
+                element.setAttribute('stroke', '#ffffff');
+                element.setAttribute('fill', '#ccc');
+                this.artboard.svg.appendChild(element);
             }
         };
         window.addEventListener('click', pointsListener);
@@ -84,22 +90,33 @@ export class EllipseFigure implements Figure<SVGEllipseElement> {
         this.cancelListener.keyEvent.on(cancel);
     }
 
-    createEditingSelection(points: [number, number][]) {
-        const [ [x1, y1] ] = points;
-        const { scrollLeft, scrollTop } = document.scrollingElement!;
+    createEditingSelection(point: PointConcerns) {
+        const { client, scroll, margin, board, zoom } = point;
+        const [x1, y1] = this.coords.formula2d(client, scroll, margin, board, zoom, false);
         const element = document.createElementNS('http://www.w3.org/2000/svg', this.name);
         element.setAttribute('stroke', '#777');
         element.setAttribute('fill', 'none');
         element.setAttribute('stroke-dasharray', '1');
         this.guides.guidesContainer!.appendChild(element);
         const onMousemove = (event: MouseEvent) => {
-            let { clientX, clientY, } = event;
-            const x2 = clientX + scrollLeft - this.artboardMove.left;
-            const y2 = clientY + scrollTop - this.artboardMove.top;
-            element.setAttribute('cx', `${ (x2 - x1) / 2 + x1 }`);
-            element.setAttribute('cy', `${ (y2 - y1) / 2 + y1 }`);
-            element.setAttribute('rx', `${ (x2 - x1) / 2 }`);
-            element.setAttribute('ry', `${ (y2 - y1) / 2 }`);
+            let { clientX, clientY, shiftKey } = event;
+            const client2: [number, number] = [clientX, clientY];
+            const [x2, y2] = this.coords.formula2d(client2, scroll, margin, board, zoom, false);
+            const cx = Math.abs(x2 - x1) / 2 + (x2 > x1 ? x1 : x2);
+            const cy = Math.abs(y2 - y1) / 2 + (y2 > y1 ? y1 : y2);
+            let rx = Math.abs(x2 - x1) / 2;
+            let ry = Math.abs(y2 - y1) / 2;
+            if (shiftKey) {
+                if (rx > ry) {
+                    rx = ry;
+                } else {
+                    ry = rx;
+                }
+            }
+            element.setAttribute('cx', `${ cx }`);
+            element.setAttribute('cy', `${ cy }`);
+            element.setAttribute('rx', `${ rx }`);
+            element.setAttribute('ry', `${ ry }`);
         };
         const onClick = (_click: MouseEvent) => {
             window.removeEventListener('mousemove', onMousemove);
